f39eaef2747de4ba95b30784a33dd65bf3cc2569
Jithin-caz <117753804+Jithin-caz@users.noreply.github.com>
2024-10-22 21:42:24 +0530

    Add project files.

---
+++ ToDoList/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace ToDoList
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void Entry_Focused(object sender, FocusEventArgs e)
+        {
+
+        }
+    }
+
+}
